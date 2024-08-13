724b2675c099765087b473cb0f77faa8b5fbf4a1
Manojkumar Patil <88649192+patilmanojkumar@users.noreply.github.com>
2024-08-13 11:57:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,18 +21,20 @@ def edit_url(url):
         return None
 
 # Streamlit app UI
-st.set_page_config(layout="wide")
+st.set_page_config(layout="centered", page_icon="📄", initial_sidebar_state="collapsed")
 st.markdown(
-    "<h1 style='text-align: center;'>Krishikosh Downloader by <a href='https://github.com/patilmanojkumar'>Manojkumar Patil</a></h1>",
+    """
+    <h1 style='text-align: center; font-size: 24px;'>Krishikosh Downloader</h1>
+    <h3 style='text-align: center;'>by <a href='https://github.com/patilmanojkumar'>Manojkumar Patil</a></h3>
+    """, 
     unsafe_allow_html=True
 )
-# Displaying the dynamic SVG banner
+
+# Dynamic SVG banner
 st.markdown(
     """
     <p align="center">
-      <a href="https://github.com/DenverCoder1/readme-typing-svg">
-        <img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=yellow&size=30&center=true&vCenter=true&width=600&height=100&lines=Download+thesis+from+Krishikosh!;" alt="Typing SVG">
-      </a>
+      <img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=yellow&size=20&center=true&vCenter=true&width=300&height=50&lines=Download+thesis+from+Krishikosh!;" alt="Typing SVG">
     </p>
     """,
     unsafe_allow_html=True
@@ -43,10 +45,12 @@ input_url = st.text_input("Paste the full-length Krishikosh URL here:")
 
 # Instructions for the user
 st.markdown("""
+<div style="font-size: 14px;">
 Please ensure that the URL you input follows the correct format:
-- Example of correct URL: `https://krishikosh.egranth.ac.in/assets/pdfjs/web/viewer.html?file=https%3A%2F%2Fkrishikosh.egranth.ac.in%2Fserver%2Fapi%2Fcore%2Fbitstreams%2Fb8a091b0-6e12-43c1-8c29-63ba25519a43%2Fcontent`
-- Incorrect URLs may not work as expected.
-""")
+<br>- Example: `https://krishikosh.egranth.ac.in/assets/pdfjs/web/viewer.html?file=https%3A%2F%2Fkrishikosh.egranth.ac.in%2Fserver%2Fapi%2Fcore%2Fbitstreams%2Fb8a091b0-6e12-43c1-8c29-63ba25519a43%2Fcontent`
+<br>- Incorrect URLs may not work as expected.
+</div>
+""", unsafe_allow_html=True)
 
 # Single button to edit the URL and follow the link
 if st.button("Download"):
@@ -54,8 +58,8 @@ if st.button("Download"):
         edited_url = edit_url(input_url)
         if edited_url:
             # Provide a link to the edited URL that the user can click
-            st.success("Link Generated successfully! Click the link below to download the file.")
-            st.markdown(f"[Download File]({edited_url})", unsafe_allow_html=True)
+            st.success("Link generated successfully! Click the link below to download the file.")
+            st.markdown(f"<a href='{edited_url}' target='_blank'>Download File</a>", unsafe_allow_html=True)
         else:
             st.error("The URL you provided does not appear to be in the correct format. Please try again.")
     else:
